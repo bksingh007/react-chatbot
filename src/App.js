ca31f76e2a6dda706154286369342666da4d634a
bksingh007 <bharat01ks@gmail.com>
2022-09-27 13:26:29 +0530

    initial changes related to chatbot app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import SimpleForm from "./components/chatbot/SimpleForm";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <SimpleForm/>
+        
+      </div>
+    );
+  }
+       
+}
+
+export default App;
